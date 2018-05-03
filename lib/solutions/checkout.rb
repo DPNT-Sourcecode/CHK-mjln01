964abcbe8f58ee390fdfaf2e315f476f64581a23
sbx_user1059 <sbx_user1059@ip-10-11-117-136.eu-west-2.compute.internal>
2018-05-03 06:27:46 +0000

    Thu May 03 06:27:46 UTC 2018

--- lib/solutions/checkout.rb
+++ lib/solutions/checkout.rb
@@ -1,14 +1,9 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
-  def checkout(skus)
-    items = [['A', 50], ['B',30], ['C', 20], ['D', 15]]
+  def checkout(skus)    
     offers = [['A', 3, 130], ['B', 2, 45]]
-    total_price = 0
-    skus.unique.each do |sku|
-      count = skus.count(sku)
-      total_price = 
-    end
+    prices = [['A', 50], ['B',30], ['C', 20], ['D', 15]]
   end
-
+  
 end
