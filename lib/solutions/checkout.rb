341357f695c0e146974b56e8c8ddfe669d3f0a5d
sbx_user1059 <sbx_user1059@ip-10-11-117-136.eu-west-2.compute.internal>
2018-05-03 06:30:46 +0000

    Thu May 03 06:30:46 UTC 2018

--- lib/solutions/checkout.rb
+++ lib/solutions/checkout.rb
@@ -5,5 +5,14 @@ class Checkout
     offers = [['A', 3, 130], ['B', 2, 45]]
     prices = [['A', 50], ['B',30], ['C', 20], ['D', 15]]
   end
-  
+
+  private
+
+  def get_offer(sku)
+    
+  end
+
+  def get_price(sku)
+  end
+
 end
