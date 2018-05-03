a617a88810ab15ceef164b15474bdacee8a383ce
sbx_user1059 <sbx_user1059@ip-10-12-1-59.eu-west-2.compute.internal>
2018-05-03 00:59:52 +0000

    Thu May 03 00:59:52 UTC 2018

--- test/hello_test.rb
+++ test/hello_test.rb
@@ -7,7 +7,7 @@ Logging.logger.root.appenders = Logging.appenders.stdout
 class HelloTest < Minitest::Test
 
   def test_hello
-    assert_match Hello.new.hello('John'), "Hello John"
+    assert_match Hello.new.hello('John'), "Hello, World!"
   end
 
 end
