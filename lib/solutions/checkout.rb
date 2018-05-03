d0571d37f13c03e3d0b170275f4e601b9fe538b4
sbx_user1059 <sbx_user1059@ip-10-11-117-136.eu-west-2.compute.internal>
2018-05-03 06:33:45 +0000

    Thu May 03 06:33:45 UTC 2018

--- lib/solutions/checkout.rb
+++ lib/solutions/checkout.rb
@@ -2,17 +2,26 @@
 class Checkout
 
   def checkout(skus)    
-    offers = [['A', 3, 130], ['B', 2, 45]]
-    prices = [['A', 50], ['B',30], ['C', 20], ['D', 15]]
+    total_price = 0
+    skus.split(//).unique.each do |squ|
+      quantity = skus.count(sku)
+      offer = get_offer(sku)
+      price = get_price(sku)
+      
+    end    
+
   end
 
   private
 
   def get_offer(sku)
-    
+    offers = [['A', 3, 130], ['B', 2, 45]]
+    offers.select { |item| item[0] == sku }.first
   end
 
   def get_price(sku)
+    prices = [['A', 50], ['B', 30], ['C', 20], ['D', 15]]
+    prices.select { |item| item[0] == sku }.first
   end
 
 end
