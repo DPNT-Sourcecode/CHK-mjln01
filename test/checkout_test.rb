4791acbbb94fe48defab753648474625714f0b3a
sbx_user1059 <sbx_user1059@ip-10-11-117-136.eu-west-2.compute.internal>
2018-05-03 07:54:12 +0000

    Thu May 03 07:54:12 UTC 2018

--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -37,10 +37,11 @@ class CheckoutTest < Minitest::Test
     # assert_equal Checkout.new.checkout("EEB"), 80
     # assert_equal Checkout.new.checkout("EEEB"), 120
 
-    # assert_equal Checkout.new.checkout("EEEEB"), 160
-    assert_equal Checkout.new.checkout("EEEEBBB"), 190
-    # assert_equal Checkout.new.checkout("EEEEBBBB"), 205
+    ## assert_equal Checkout.new.checkout("EEEEB"), 160
     
+
+    assert_equal Checkout.new.checkout("EEEEBBB"), 90
+    # assert_equal Checkout.new.checkout("EEEEBBBB"), 205    
     # assert_equal Checkout.new.checkout("ABCDEABCDE"), 280
     # assert_equal Checkout.new.checkout("CCADDEEBBA"), 280
     # assert_equal Checkout.new.checkout("AAAAAEEBAAABB"), 455
